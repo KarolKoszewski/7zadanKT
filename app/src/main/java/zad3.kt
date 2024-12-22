535d37b07460ce2221114e44478911db7990f728
Karol <karol144y@gmail.com>
2024-12-22 22:09:44 +0100

    Dodaje opcje dla uzytkownika czy chce dodac książkę czy znaleść jakąś oraz dodaje metodę która umożliwia dodane nowej książki

--- app/src/main/java/zad3.kt
+++ app/src/main/java/zad3.kt
@@ -12,20 +12,39 @@ fun main() {
     println("Wszystkie książki:")
     books.forEach { println("${it.title} by ${it.author}, Year: ${it.year}") }
 
+    println("==============================================================")
 
+    println("Czy chcesz dodać nową książkę?(1-tak, 2-nie)")
+    val addBook = readLine()!!.toInt()
 
+    if(addBook == 2){
 
+        println("Podaj autora książki: ")
+        val author = readLine()!!.toString()
+        println("Książki autora ${author}}")
+        books.filter { it.author == author }.forEach { println(it.title) }
 
-    println("Podaj autora książki: ")
-    val author = readLine()!!.toString()
-    println("Książki autora ${author}}")
-    books.filter { it.author == author }.forEach { println(it.title) }
+        println("Podaj rok wydania książki: ")
+        val y = readLine()!!.toInt()
+        println("Książki wydane w ${y} roku:")
+        books.filter { it.year == y }.forEach { println(it.title) }
 
-    println("Podaj rok wydania książki: ")
-    val y = readLine()!!.toInt()
-    println("Książki wydane w ${y} roku:")
-    books.filter { it.year == y }.forEach { println(it.title) }
+        println("Książki posortowane alfabetycznie:")
+        books.sortedBy { it.title }.forEach { println(it.title) }
 
-    println("Książki posortowane alfabetycznie:")
-    books.sortedBy { it.title }.forEach { println(it.title) }
+    }else if(addBook == 1){
+        println("Podaj tytuł książki: ")
+        val title = readLine()!!.toString()
+
+        println("Podaj autora książki: ")
+        val author = readLine()!!.toString()
+
+        println("Podaj rok wydania książki: ")
+        val year = readLine()!!.toInt()
+
+        books.add(Book(title, author, year))
+        println("Wszystkie książki:")
+        books.forEach { println("${it.title} by ${it.author}, Year: ${it.year}") }
+    }
+
 }
