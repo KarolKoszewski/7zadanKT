361c06d800cbae6a768c133fd012924e852b1d97
Karol <karol144y@gmail.com>
2024-12-22 21:57:46 +0100

    Dodaje fitrowanie książek po autorze

--- app/src/main/java/zad3.kt
+++ app/src/main/java/zad3.kt
@@ -9,7 +9,12 @@ fun main() {
     books.add(Book("1984", "George Orwell", 1949))
     books.add(Book("Zbrodnia i kara", "Fyodor Dostoevsky", 1866))
     books.add(Book("Mistrz i Małgorzata", "Michaił Bułhakow", 1966))
+    println("Wszystkie książki:")
+    books.forEach { println("${it.title} by ${it.author}, Year: ${it.year}") }
 
-
+    println("Podaj autora książki: ")
+    val author = readLine()!!.toString()
+    println("Książki autora ${author}}")
+    books.filter { it.author == author }.forEach { println(it.title) }
 
 }
