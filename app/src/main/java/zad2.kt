498fe5cdfcd761c34285d91067d0bcea2c890351
szymon <mail@example.com>
2024-12-22 20:25:58 +0100

    dodanie cytatow

---
+++ app/src/main/java/zad2.kt
@@ -0,0 +1,11 @@
+import kotlin.random.Random
+object Quotes{
+    val quotes = mutableListOf(
+        "Technika się siły boi",
+        "You tell me 'Good Luck'. I don't need luck to perform. I decide when to perform good. I'm playing to win and to win. I will destory you",
+        "Ty to chyba Chińczyk jesteś",
+        "Anastazja przestań pisac",
+    )
+}
+fun main()
+{}
