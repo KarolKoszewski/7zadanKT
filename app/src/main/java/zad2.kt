88df791678089b7ee51203e8735a6923a900bdbb
szymon <mail@example.com>
2024-12-22 20:42:20 +0100

    Dodanie losowania cytatow

--- app/src/main/java/zad2.kt
+++ app/src/main/java/zad2.kt
@@ -4,8 +4,22 @@ object Quotes{
         "Technika się siły boi",
         "You tell me 'Good Luck'. I don't need luck to perform. I decide when to perform good. I'm playing to win and to win. I will destory you",
         "Ty to chyba Chińczyk jesteś",
-        "Anastazja przestań pisac",
+        "Anastazja przestańpisac",
     )
+
+    fun getRandomquote(): String
+    {
+        if(quotes.isEmpty())
+        {
+            return "Brak dostępnych cytatów"
+        }
+        val randomQuote=Random.nextInt(quotes.size)
+        return quotes[randomQuote]
+    }
+
 }
 fun main()
-{}
+{
+    println("Losowy cytat: ${Quotes.getRandomquote()}")
+
+}
