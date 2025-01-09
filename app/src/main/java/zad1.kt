59f9e443e88f06cccd8719ca7bf93e3109a5ac37
KarolKoszewski <135307962+KarolKoszewski@users.noreply.github.com>
2025-01-09 13:52:12 +0100

    ukonczenie zadania pierwszego 1

--- app/src/main/java/zad1.kt
+++ app/src/main/java/zad1.kt
@@ -15,16 +15,35 @@ class Coin{
     }
 }
 class Dice{
+    val b = Random.nextInt(1,7)
+    val jeden = "Liczba oczek 1"
+    val dwa = "Liczba oczek 2"
+    val trzy = "Liczba oczek 3"
+    val cztery = "Liczba oczek 4"
+    val piec = "Liczba oczek 5"
+    val szesc = "Liczba oczek 6"
+
+    fun rzutKostka(){
+        when(b){
+            1 -> println(jeden)
+            2 -> println(dwa)
+            3 -> println(trzy)
+            4 -> println(cztery)
+            5 -> println(piec)
+            6 -> println(szesc)
+        }
+    }
 
 }
 
 fun main(){
     println("Chcesz losowac kostka czy moneta(1-moneta, 2-kostka): ")
     val moneta1 = Coin()
+    val kostka1 = Dice()
     val wyborDiceOrCoin = readLine()!!.toInt()
     if(wyborDiceOrCoin == 1){
         moneta1.tailOrHead()
     }else if(wyborDiceOrCoin ==2){
-        println("Dice")
+        kostka1.rzutKostka()
     }
 }
